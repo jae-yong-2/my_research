eab9621a3572634510733fd19cb35f5cc8b10eba
jea-yong-2 <ljy8972@naver.com>
2024-02-05 19:16:18 +0900

    :white_check_mark: Background, foreground 기능 구현, ios, android 테스트 필요

--- lib/foreground_service.dart
+++ lib/foreground_service.dart
@@ -1,143 +1,96 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_foreground_service/flutter_foreground_service.dart';
 import 'dart:async';
-import 'dart:io';
-import 'package:activity_recognition_flutter/activity_recognition_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 
-class ForegroundServiceAPI extends StatelessWidget {
+class ForegroundServiceAPI extends StatefulWidget {
   const ForegroundServiceAPI({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyTask(),
-    );
-  }
+  State<ForegroundServiceAPI> createState() => _ForegroundServiceAPIState();
 }
 
-class MyTask extends StatefulWidget {
-  @override
-  _MyTaskState createState() => _MyTaskState();
-}
-
-class _MyTaskState extends State<MyTask> {
-  String _statusMesssge = '작동안함';
-
-  StreamSubscription<ActivityEvent>? activityStreamSubscription;
-  final List<ActivityEvent> _events = [];
-  ActivityRecognition activityRecognition = ActivityRecognition();
-
-  @override
-  void initState() {
-    super.initState();
-    startForegroundService();
-    _init();
-    _events.add(ActivityEvent.unknown());
-  }
-
-  @override
-  void dispose() {
-    activityStreamSubscription?.cancel();
-    super.dispose();
-  }
-
-  void startForegroundService() async {
-    ForegroundService().start();
-    _statusMesssge = '백그라운드 시작';
-  }
-  void stopForegroundService() async {
-    ForegroundService().stop();
-    _statusMesssge = '백그라운드 종료';
-  }
-
-  void _init() async {
-    // Android requires explicitly asking permission
-    if (Platform.isAndroid) {
-      if (await Permission.activityRecognition.request().isGranted) {
-        _startTracking();
-      }
-    }
-
-    // iOS does not
-    else {
-      _startTracking();
-    }
-  }
-
-  void _startTracking() {
-    activityStreamSubscription = activityRecognition
-        .activityStream(runForegroundService: true)
-        .listen(onData, onError: onError);
-  }
-
-  void onData(ActivityEvent activityEvent) {
-    print(activityEvent);
-    setState(() {
-      _events.add(activityEvent);
-    });
-  }
-
-  void onError(Object error) {
-    print('ERROR - $error');
-  }
-
-  Icon _activityIcon(ActivityType type) {
-    switch (type) {
-      case ActivityType.WALKING:
-        return Icon(Icons.directions_walk);
-      case ActivityType.IN_VEHICLE:
-        return Icon(Icons.car_rental);
-      case ActivityType.ON_BICYCLE:
-        return Icon(Icons.pedal_bike);
-      case ActivityType.ON_FOOT:
-        return Icon(Icons.directions_walk);
-      case ActivityType.RUNNING:
-        return Icon(Icons.run_circle);
-      case ActivityType.STILL:
-        return Icon(Icons.cancel_outlined);
-      case ActivityType.TILTING:
-        return Icon(Icons.redo);
-      default:
-        return Icon(Icons.device_unknown);
-    }
-  }
-
+class _ForegroundServiceAPIState extends State<ForegroundServiceAPI> {
+  String text = "Stop Service";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_statusMesssge),
-      ),
-      body:
-        Center(
-          child: Row(
-            children: [
-              Expanded( // 또는 Flexible
-                child: ListView.builder(
-                    itemCount: _events.length,
-                    reverse: true,
-                    itemBuilder: (_, int idx) {
-                      var activity = _events[idx];
-                      return ListTile(
-                        leading: _activityIcon(activity.type),
-                        title: Text(
-                            '${activity.type.toString().split('.').last} (${activity.confidence}%)'),
-                        trailing: Text(activity.timeStamp
-                            .toString()
-                            .split(' ')
-                            .last
-                            .split('.')
-                            .first),
-                      );
-                    }
-                  // ListView.builder 코드
-                ),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              FlutterBackgroundService().invoke("setAsForeground");
+            }, child: const Text("Foreground Service")),
+            ElevatedButton(onPressed: (){
+              FlutterBackgroundService().invoke("setAsBackground");
+            }, child: const Text("Background Service")),
+            ElevatedButton(onPressed: () async {
+              final service = FlutterBackgroundService();
+              bool isRunning = await service.isRunning();
+              if(isRunning){
+
+                service.invoke("stopService");
+              }else{
+                service.startService();
+              }
+              if(!isRunning){
+                text="Stop Service";
+              }{
+                text= "Start Service";
+              }
+              setState(() {});
+            }, child: Text(text)),
+          ],
         ),
+      ),
+
     );
   }
+
+}
+
+Future<void> initializeService() async{
+  final service = FlutterBackgroundService();
+  await service.configure(iosConfiguration: IosConfiguration(
+    autoStart: true,
+    onForeground: onStart,
+    onBackground: onIosBackground,
+  ),
+      androidConfiguration: AndroidConfiguration(onStart: onStart, isForegroundMode: true, autoStart: true));
+}
+
+@pragma('vm:entry-point')
+FutureOr<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized(); // 필요한 경우 주석 처리
+  return true;
+}
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+  if( service is AndroidServiceInstance){
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+  Timer.periodic(Duration(seconds:  1), (timer) async {
+    if(service is AndroidServiceInstance){
+      if (await service.isForegroundService()){
+        service.setForegroundNotificationInfo(title: "Script ACADEMY", content: "sub my cahnnel");
+      }
+    }
+    //사용자가 모르게 백그라운드 실행됨
+    print('background service running');
+    service.invoke('update');
+  });
 }
