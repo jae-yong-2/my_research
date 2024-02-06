b7b7ea1baa2f719d5910e9ddb03ecc4684cc0dd7
jea-yong-2 <ljy8972@naver.com>
2024-02-06 14:18:25 +0900

    :white_check_mark: 푸시알람으로 업데이트

--- lib/foreground_service.dart
+++ lib/foreground_service.dart
@@ -1,96 +1,153 @@
 import 'dart:async';
-import 'dart:ui';
+import 'dart:isolate';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 class ForegroundServiceAPI extends StatefulWidget {
   const ForegroundServiceAPI({super.key});
 
   @override
-  State<ForegroundServiceAPI> createState() => _ForegroundServiceAPIState();
+  State<ForegroundServiceAPI> createState() => _ForegroundServiceState();
 }
 
-class _ForegroundServiceAPIState extends State<ForegroundServiceAPI> {
-  String text = "Stop Service";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: (){
-              FlutterBackgroundService().invoke("setAsForeground");
-            }, child: const Text("Foreground Service")),
-            ElevatedButton(onPressed: (){
-              FlutterBackgroundService().invoke("setAsBackground");
-            }, child: const Text("Background Service")),
-            ElevatedButton(onPressed: () async {
-              final service = FlutterBackgroundService();
-              bool isRunning = await service.isRunning();
-              if(isRunning){
+class _ForegroundServiceState extends State<ForegroundServiceAPI> {
 
-                service.invoke("stopService");
-              }else{
-                service.startService();
-              }
-              if(!isRunning){
-                text="Stop Service";
-              }{
-                text= "Start Service";
-              }
-              setState(() {});
-            }, child: Text(text)),
-          ],
+  void _initForegroundTask() {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription: 'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+        iconData: const NotificationIconData(
+          resType: ResourceType.mipmap,
+          resPrefix: ResourcePrefix.ic,
+          name: 'launcher',
         ),
+        buttons: [
+          const NotificationButton(id: 'sendButton', text: 'Send'),
+          const NotificationButton(id: 'testButton', text: 'Test'),
+        ],
       ),
-
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        interval: 5000,
+        isOnceEvent: false,
+        autoRunOnBoot: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
     );
   }
 
-}
-
-Future<void> initializeService() async{
-  final service = FlutterBackgroundService();
-  await service.configure(iosConfiguration: IosConfiguration(
-    autoStart: true,
-    onForeground: onStart,
-    onBackground: onIosBackground,
-  ),
-      androidConfiguration: AndroidConfiguration(onStart: onStart, isForegroundMode: true, autoStart: true));
-}
-
-@pragma('vm:entry-point')
-FutureOr<bool> onIosBackground(ServiceInstance service) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  DartPluginRegistrant.ensureInitialized(); // 필요한 경우 주석 처리
-  return true;
-}
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) async {
-  DartPluginRegistrant.ensureInitialized();
-  if( service is AndroidServiceInstance){
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
+  @override
+  void initState() {
+    super.initState();
+    _initForegroundTask();
+    Future.microtask(() async{
+      isRun = await FlutterForegroundTask.isRunningService;
+      if(this.mounted) return;
+      setState(() {});
     });
   }
-  service.on('stopService').listen((event) {
-    service.stopSelf();
+
+  bool isRun =false;
+
+  @override
+  WithForegroundTask build(BuildContext context) => WithForegroundTask(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Foreground Task 2023"),),
+        body: Center(
+          child: TextButton(
+            child: Text("update"),
+            onPressed: () async{
+              FlutterForegroundTask.updateService(
+                notificationTitle: "update",
+                notificationText: "update_txt",
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: !isRun
+              ?Icon(Icons.play_arrow)
+              :Icon(Icons.stop,color: Colors.red,),
+          onPressed: () async{
+            if(await FlutterForegroundTask.isRunningService) {
+              await FlutterForegroundTask.clearAllData();
+              await FlutterForegroundTask.stopService();
+              print('종료');
+            }else{
+              await FlutterForegroundTask.startService(
+                  notificationTitle: "test",
+                  notificationText: "foreground task",
+                  callback: startCallback
+              );
+            }
+            isRun = await FlutterForegroundTask.isRunningService;
+            setState(() {
+            });
+          },
+        ),
+      )
+  );
+}
+
+// The callback function should always be a top-level function.
+@pragma('vm:entry-point')
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  print('실행');
+  Timer.periodic(Duration(seconds: 1), (timer) {
+    print('안녕');
   });
-  Timer.periodic(Duration(seconds:  1), (timer) async {
-    if(service is AndroidServiceInstance){
-      if (await service.isForegroundService()){
-        service.setForegroundNotificationInfo(title: "Script ACADEMY", content: "sub my cahnnel");
-      }
-    }
-    //사용자가 모르게 백그라운드 실행됨
-    print('background service running');
-    service.invoke('update');
-  });
+  FlutterForegroundTask.setTaskHandler(FirstTaskHandler());
+}
+
+class FirstTaskHandler extends TaskHandler {
+  SendPort? _sendPort;
+
+  // Called when the task is started.
+  @override
+  void onStart(DateTime timestamp, SendPort? sendPort) async {
+    _sendPort = sendPort;
+
+    // You can use the getData function to get the stored data.
+    final customData =
+    await FlutterForegroundTask.getData<String>(key: 'customData');
+    print('customData: $customData');
+  }
+
+  // Called every [interval] milliseconds in [ForegroundTaskOptions].
+  @override
+  void onRepeatEvent(DateTime timestamp, SendPort? sendPort) async {
+    // Send data to the main isolate.
+    sendPort?.send(timestamp);
+  }
+
+  // Called when the notification button on the Android platform is pressed.
+  @override
+  void onDestroy(DateTime timestamp, SendPort? sendPort) async {
+
+  }
+
+  // Called when the notification button on the Android platform is pressed.
+  @override
+  void onNotificationButtonPressed(String id) {
+    print('onNotificationButtonPressed >> $id');
+  }
+
+
+  @override
+  void onNotificationPressed() {
+
+    //FlutterForegroundTask.launchApp("/resume-route");
+    _sendPort?.send('onNotificationPressed');
+  }
 }
