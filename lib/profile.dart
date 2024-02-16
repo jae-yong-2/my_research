3850440392b00abebb11b87c10b0c45fc576e7e1
jea-yong-2 <ljy8972@naver.com>
2024-02-16 15:45:07 +0900

    :lipstick: 나이 성별 등등 삭제

--- lib/profile.dart
+++ lib/profile.dart
@@ -10,41 +10,7 @@ class Profile extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '성별',
-                helperText: '성별을 입력하세요',
-                border: OutlineInputBorder(), // 테두리 스타일 적용
-              ),
-            ),
             SizedBox(height: 10), // 여백 추가
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '나이',
-                helperText: '나이를 숫자로 입력하세요',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '키 (cm)',
-                helperText: '키를 cm 단위로 입력하세요',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '몸무게 (kg)',
-                helperText: '몸무게를 kg 단위로 입력하세요',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 10),
             TextFormField(
               decoration: InputDecoration(
                 labelText: '평소 습관 및 자세',
