ef1a2a8abc9754d037150500dfca2a6471b8bded
paintedblue <pinkclover22@naver.com>
2024-03-06 14:20:29 +0900

    sharedpreference 디바이스 아이디 저장

--- lib/saveData.dart
+++ lib/saveData.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class SaveData {
   final databaseRef = FirebaseDatabase.instance.ref();
@@ -27,4 +29,15 @@ class SaveData {
       return null;
     }
   }
+  // SharedPreferences에 디바이스 ID를 저장하는 함수
+  /* saveDeviceId 함수는 SharedPreferences 인스턴스를 비동기적으로 가져온 다음,
+     setString 메서드를 사용하여 'key'라는 키로 deviceId 값을 저장합니다.
+     이 함수를 호출하여 디바이스 ID를 SharedPreferences에 저장할 수 있습니다.
+   */
+
+  Future<void> saveDeviceId(String deviceId) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('key', deviceId);
+  }
 }
+
